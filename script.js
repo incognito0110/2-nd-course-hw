a7aaf6f3f20ecab2413c51dffaebcc285c454fe9
artem <vladpolugarov293@gmail.com>
2025-12-28 17:22:58 +0300

    added js

---
+++ script.js
@@ -0,0 +1,66 @@
+/*задание 1*/
+
+// let a = 10;
+// alert(a);
+
+// let b = 20;
+// alert(b);
+
+/*задание 2*/
+
+// let firstIPhoneYear = 2007;
+// alert(firstIPhoneYear);
+
+/*задание 3*/
+
+// let Creator = "Брендан Эйх";
+// alert(Creator);
+
+/*задание 4*/
+
+// let first = 10;
+// let second = 2;
+// let amount = first + second;
+// alert(amount);
+
+/*задание 5*/
+
+// let result = 2**5;
+// alert(result);
+
+/*задание 6*/
+
+// let a = 9;
+// let b = 2;
+// let result = a % b;
+// alert(result);
+
+/*задание 7*/
+
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num++;
+// num--;
+// alert(num);
+
+
+/*задание 8*/
+
+// let age = prompt("Сколько вам лет?");
+// alert(age);
+
+/*задание 9*/
+
+// const user = {
+//     name: "alex",
+//     age: 24,
+//     isAdmin: true,
+// }
+
+/*задание 10*/
+
+// let name = prompt("Как твое имя?");
+// alert(`Привет, ${name}!`);

7b5dacede69fb94ab1f05f6e5753cb7cd3b48a3d
artem <vladpolugarov293@gmail.com>
2026-01-14 17:46:37 +0300

    added hw-2

---
+++ hw-2.js
@@ -0,0 +1,85 @@
+/*задание 1*/
+
+// let answer = prompt("Введите пароль");
+
+// if (answer === "пароль" || answer === "пароль" ) {
+//     alert("Пароль введен верно");
+// } else {
+//     alert("Пароль введен неправильно");
+// }
+
+/*задание 2*/
+
+// let c = 25;
+
+// if (c > 0 && c < 10) {
+//     alert("Верно");
+// } else {
+//     alert("Неверно");
+// }
+
+/*задание 3*/
+
+// let d = 1;
+// let b = 1001;
+
+// if (d > 100 || b > 100) {
+//     alert("Верно")
+// } else {
+//     alert("Неверно")
+// }
+
+/*задание 4*/
+
+// let a = '2';
+// let b = '3';
+
+// alert(Number(a) + Number(b));
+
+/*задание 5*/
+
+// let monthNumber = prompt("Число месяца года");
+
+
+// switch (monthNumber) {
+//   case '1':
+//     alert('Зима');
+//     break;
+//   case '2':
+//     alert('Зима');
+//     break;
+//   case '3':
+//     alert('Весна');
+//     break;
+//   case '4':
+//     alert('Весна');
+//     break;
+//   case '5':
+//     alert('Весна');
+//     break;
+//   case '6':
+//     alert('Лето');
+//     break;
+//   case '7':
+//     alert('Лето');
+//     break;
+//   case '8':
+//     alert('Лето');
+//     break;
+//   case '9':
+//     alert('Осень');
+//     break;
+//   case '10':
+//     alert('Осень');
+//     break;
+//   case '11':
+//     alert('Осень');
+//     break;
+//   case '12':
+//     alert('Зима');
+//     break;
+
+//   default:
+//     alert('Некорректный номер месяца. Месяц должен быть от 1 до 12.');
+//     break;
+// }

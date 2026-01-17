6500e85f57dd2847b035d1919227d2141ac8da28
artem <vladpolugarov293@gmail.com>
2026-01-17 23:27:26 +0300

    fix 5 quest

--- hw-2.js
+++ hw-2.js
@@ -45,37 +45,37 @@
 
 /*задание 5*/
 
-// let monthNumber = 1;
+// let monthNumber = 13;
 
-// if (monthNumber > 13) {
-
+// if (monthNumber >= 13) {
+//   console.log("Неверный номер месяца. Программа остановлена.");
 // } else {
-//     switch (monthNumber) {
-//         case 12:
-//         case 1:
-//         case 2:
-//             console.log("зима");
-//             break;
-//         case 3:
-//         case 4:
-//         case 5:
-//             console.log("весна");
-//             break;
 
-//         case 6:
-//         case 7:
-//         case 8:
-//             console.log("лето");
-//             break;
+//   switch (monthNumber) {
+//     case 12:
+//     case 1:
+//     case 2:
+//       console.log("Зима");
+//       break;
 
-//         case 9:
-//         case 10:
-//         case 11:
-//             console.log("осень");
-//             break;
-//         default:
-//             console.log("Неверный номер месяца");
-//     }
+//     case 3:
+//     case 4:
+//     case 5:
+//       console.log("Весна");
+//       break;
+
+//     case 6:
+//     case 7:
+//     case 8:
+//       console.log("Лето");
+//       break;
+
+//     case 9:
+//     case 10:
+//     case 11:
+//       console.log("Осень");
+//       break;
+//   }
 // }
 
 
